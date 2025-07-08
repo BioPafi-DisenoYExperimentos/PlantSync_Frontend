4440ba1976961153a1c73f2b563373218887e824
Renzo <u20231d974@upc.edu.pe>
2025-07-08 04:55:03 -0500

    feat: updated backend link
    Updated backend url for weather service

--- src/app/features/weather/services/real-weather.service.ts
+++ src/app/features/weather/services/real-weather.service.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { WeatherStatus } from '../model/weather-status.model';
+import {environment} from "../../../../environments/environment.development";
 
 @Injectable({
     providedIn: 'root'
 })
 export class RealWeatherService {
-    private apiKey = 'f2aae48671e29e04c682372baceebe65';
-    private baseUrl = 'http://localhost:8080/api/v1/weather/city'; // o tu URL en Azure
+    private baseUrl = `${environment.BASE_URL}/weather/city`;
 
     constructor(private http: HttpClient) {}
 
     getWeatherByCity(city: string): Observable<WeatherStatus> {
-        const url = `${this.baseUrl}?city=${city}`;
+        const url = `${this.baseUrl}?city=${encodeURIComponent(city)}`;
         return this.http.get<any>(url).pipe(
             map(data => ({
                 id: 0,
